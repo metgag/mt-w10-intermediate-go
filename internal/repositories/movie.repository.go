41fb1ed0f7f2f83b3587b36c0a40bb007fe4593b
metgag <moreehigh1@gmail.com>
2025-09-04 11:20:34 +0700

    refactor: mv ctx handler to movie.handler and change from void func to return ctag, error

--- internal/repositories/movie.repository.go
+++ internal/repositories/movie.repository.go
@@ -2,10 +2,10 @@ package repositories
 
 import (
 	"fmt"
-	"net/http"
 	"reflect"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/metgag/mt-w10d1/internal/models"
 )
@@ -18,7 +18,7 @@ func NewMovieRepository(dbpool *pgxpool.Pool) *MovieRepository {
 	return &MovieRepository{dbpool: dbpool}
 }
 
-func (m *MovieRepository) UpdateMovieData(ctx *gin.Context, updateMovie models.Foo, idParam int) {
+func (m *MovieRepository) UpdateMovieData(ctx *gin.Context, updateMovie models.Foo, idParam int) (pgconn.CommandTag, error) {
 	rt := reflect.TypeOf(updateMovie)
 	rv := reflect.ValueOf(updateMovie)
 
@@ -37,11 +37,6 @@ func (m *MovieRepository) UpdateMovieData(ctx *gin.Context, updateMovie models.F
 	}
 
 	foo += fmt.Sprintf(" updated_at = current_timestamp WHERE id = %d;", idParam)
-	// fmt.Println(foo)
 
-	m.dbpool.Exec(ctx, foo)
-	ctx.JSON(http.StatusCreated, gin.H{
-		"result":  fmt.Sprintf("update ID: %d", idParam),
-		"success": true,
-	})
+	return m.dbpool.Exec(ctx, foo)
 }
