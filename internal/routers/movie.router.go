bcc99fceef92301554d2784dd46f84d5b41fd9c8
metgag <moreehigh1@gmail.com>
2025-09-04 08:38:15 +0700

    feat: split PATCH method, to update movie

---
+++ internal/routers/movie.router.go
@@ -0,0 +1,15 @@
+package routers
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/metgag/mt-w10d1/internal/handlers"
+	"github.com/metgag/mt-w10d1/internal/repositories"
+)
+
+func InitMovieRouter(r *gin.Engine, dbpool *pgxpool.Pool) {
+	mr := repositories.NewMovieRepository(dbpool)
+	mh := handlers.NewMovieHandler(mr)
+
+	r.PATCH("/movie", mh.UpdateMovie)
+}
