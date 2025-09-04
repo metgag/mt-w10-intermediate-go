f34c55abdaae9d9f35d7bf5703f78f895a054105
metgag <moreehigh1@gmail.com>
2025-09-04 08:36:04 +0700

    feat: split router func InitRouter

---
+++ internal/routers/router.go
@@ -0,0 +1,15 @@
+package routers
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func InitRouter(dbpool *pgxpool.Pool) *gin.Engine {
+	r := gin.Default()
+
+	// InitAuthRouter(r, dbpool)
+	InitMovieRouter(r, dbpool)
+
+	return r
+}
